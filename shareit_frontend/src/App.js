d4594da0d96f376d68a5c080bbaa06e949f5cad4
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-28 20:58:17 +0430

    Add react

--- shareit_frontend/src/App.js
+++ shareit_frontend/src/App.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>
+        <h1 className="text-3xl font-bold underline">
             Hello world!
-        </div>
+        </h1>
     );
 }
 
-export default App;
+export default App;  
