fa89341e9ed4ca7005ff931291b4908fa833d846
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-29 19:52:40 +0430

    Add routes in app file

--- shareit_frontend/src/App.js
+++ shareit_frontend/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './container/Home';
+import Login from './components/Login';
 
 const App = () => {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <Routes>
+            <Route path='/*' element={<Home />} />
+            <Route path='login' element={<Login />} />
+        </Routes>
     );
 }
 
