5cdbc9caa9a5e93f619a5f7ded3b0b2b4e93c8e0
iqbal-elham <iqbal.elham7@gmail.com>
2023-03-29 19:53:06 +0430

    Add router and google provider in index.js

--- shareit_frontend/src/index.js
+++ shareit_frontend/src/index.js
@@ -1,6 +1,17 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+        <App />
+      </GoogleOAuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
