76a4092423b93b56dad5626515853f9058f1f84d
iqbal-elham <iqbal.elham7@gmail.com>
2023-04-02 21:54:27 +0430

    import userprofile nad dependencies in home

--- shareit_frontend/src/container/Home.jsx
+++ shareit_frontend/src/container/Home.jsx
@@ -1,4 +1,12 @@
 import React, { useState, useRef, useEffect } from 'react';
+import { HiMenuAlit4 } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Routes, Route } from 'react-router-dom';
+import { SideBar, useProfile } from '../components';
+import { client } from '../client';
+import logo from '../assets/logo.png';
+import Pins from './Pins';
+
 
 const Home = () => {
     return (
